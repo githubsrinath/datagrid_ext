da5dca7a41525087fdf3a057d626756f173c24bd
Aaron Gong <ais-one@users.noreply.github.com>
2015-10-27 08:23:55 +0800

    Create cdn.php

---
+++ cdn.php
@@ -0,0 +1,43 @@
+<!-- UNUSED
+<link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/chosen/1.4.2/chosen.min.css'>
+<script src='//cdnjs.cloudflare.com/ajax/libs/chosen/1.4.2/chosen.jquery.min.js'></script>
+
+<link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/select2/3.5.2/select2.min.css'>
+<link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/select2/3.5.2/select2-bootstrap.min.css'>
+<script src='//cdnjs.cloudflare.com/ajax/libs/select2/3.5.2/select2.min.js'></script>
+
+<link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.1/css/selectize.bootstrap3.min.css'>
+<script src='//cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.1/js/standalone/selectize.min.js'></script>
+
+<style type="text/css">
+.autocomplete-selected { background: #ccc; color: #000; }
+.autocomplete-suggestion { cursor: pointer; overflow: hidden; white-space: nowrap; padding: 2px 5px; }
+.autocomplete-suggestions { border: 1px solid #fff; background: #eee; overflow: auto; }
+</style>
+<script src='//cdnjs.cloudflare.com/ajax/libs/jquery.devbridge-autocomplete/1.2.23/jquery.autocomplete.min.js'></script>
+
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+<link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/css/bootstrap-datetimepicker.min.css'>
+<script src='//cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js'></script>
+UNUSED -->
+<?php if ($_GET['css']) { ?>
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+<link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/css/select2.min.css'>
+<link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.4.5/jquery.datetimepicker.min.css'>
+<link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.1/bootstrap3-editable/css/bootstrap-editable.css'>
+<?php } ?>
+
+<?php if ($_GET['js']) { ?>
+<script type='text/javascript' src='//code.jquery.com/jquery-2.1.4.min.js'></script>
+<script type='text/javascript' src='//netdna.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js'></script>
+<script type='text/javascript' src='//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.min.js'></script>
+<script type='text/javascript' src='//cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.4.5/jquery.datetimepicker.min.js'></script>
+
+<script type='text/javascript' src='//cdnjs.cloudflare.com/ajax/libs/signature_pad/1.4.0/signature_pad.min.js'></script>
+<script type='text/javascript' src='//cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.4/webcam.min.js'></script>
+<!-- script type='text/javascript' src='//cdnjs.cloudflare.com/ajax/libs/jeditable.js/1.7.3/jeditable.min.js'></script -->
+<script type='text/javascript' src='//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.1/bootstrap3-editable/js/bootstrap-editable.min.js'></script>
+<!-- script src='//test.era.sg/assets/jq-signature.min.js'></script -->
+<?php } ?>
